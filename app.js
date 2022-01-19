aef041c05079729f1fdc8df603fe5a2c731a1e46
Anastassy Cap <anacap100@gmail.com>
2022-01-18 20:10:26 -0500

    basic mongodb connection + firebase auth

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+const connectToCluster = require("./database/mongodb");
 const express = require('express');
 const app = express();
 const routes = require('./routes/index');
@@ -10,5 +12,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', routes);
 
 app.listen(port, () => {
+    const mongoClient = connectToCluster(process.env.MONGO_CLUSTER_URL).then(r => console.log("success..."));
     console.log(`Server listening on port ${port}...`)
 });
