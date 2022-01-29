1fa76cca1688c8c43fddc1b5d597bea13426af97
p-carrier <p-carrier@users.noreply.github.com>
2022-01-29 16:55:44 -0500

    create administrator basic structure & test

---
+++ domain/administrator.js
@@ -0,0 +1,21 @@
+class Administrator {
+  constructor() {
+
+  }
+
+  viewDoctors() {
+    throw new Error(`${this.viewDoctors.name} is not implemented.`);
+  }
+
+  viewUsers() {
+    throw new Error(`${this.viewDoctors.name} is not implemented.`);
+  }
+
+  setUserRole(user, role) {
+    throw new Error(`${this.updateStatus.name} is not implemented.`);
+  }
+
+  assignPatient(patient, doctor) {
+    throw new Error(`${this.assignPatient.name} is not implemented.`);
+  }
+}
---
+++ domain/administrator.test.js
@@ -0,0 +1,15 @@
+const Administrator = require('./administrator');
+
+describe('test value object', () => {
+
+})
+
+describe('test Administrator object', () => {
+  describe('Administrator creation tests', () => {
+
+  })
+
+  describe('Administrator method tests', () => {
+    
+  })
+})
