554ab840d1bb8bff41efcb50d1d484c15f5bebc2
Anastassy Cap <anacap100@gmail.com>
2022-02-01 22:01:07 -0500

    patients are saved in the db

--- routes/index.js
+++ routes/index.js
@@ -4,27 +4,43 @@ const router = express.Router();
 const UserId = require('../domain/user').UserId
 const Patient = require('../domain/patient');
 
-router.get('', function (req, res) {
-   res.send("Hello world");
-});
-
-router.get('/patient/:userId/profile', (req, res) => {
+router.get('/patient/:userId/profile', async (req, res) => {
    try {
+
+      const mongodb = await req.app.locals.mongodb;
+      const response = await mongodb.db('test').collection('users').findOne({ uid: req.params.userId });
+
       const userId = new UserId(req.params.userId);
       const patient = new Patient(userId);
       patient.viewProfile();
-      res.status(201).json();
+
+      res.status(200).send(response);
    } catch (error) {
       res.status(400).json({ error: error.message });
    }
 })
 
-router.post('/patient/:userId/profile', (req, res) => {
+router.post('/patient/:userId/profile', async function (req, res) {
+
+   // temporary code that will need to be replaced with the correct info from sign up form
+
    try {
-      const userId = new UserId(req.params.userId);
-      const patient = new Patient(userId);  // TODO: add all information
-      patient.updateProfile();
-      res.status(201).send();
+      const userObj = {
+         email: req.body.email,
+         firstName: req.body.firstName,
+         lastName: req.body.lastName,
+         uid: req.params.userId,
+         userType: req.body.userType,
+         healthInsuranceNumber: req.body.healthInsuranceNumber
+      };
+
+      const mongodb = await req.app.locals.mongodb;
+      const response = await mongodb.db('test').collection('users').insertOne(userObj);
+
+      // const userId = new UserId(req.params.userId);
+      // const patient = new Patient(req.body.uid, req.body.firstName, req.body.lastName, req.body.userType, req.body.phoneNumber);
+      // patient.updateProfile();
+      res.status(201).send(response);
    } catch (error) {
       res.status(400).json({ error: error.message });
    }
