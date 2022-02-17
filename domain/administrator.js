53bc82521004a27af3c2d2219997d24ccdf8b05c
Anastassy Cap <anacap100@gmail.com>
2022-02-16 19:10:14 -0500

    removed redundant throw clauses

--- domain/administrator.js
+++ domain/administrator.js
@@ -12,111 +12,63 @@ class Administrator {
   }
 
   async viewDoctors() {
-    try {
       await this.verifyAdmin();
       return await this.adminRepository.fetchPendingDoctors(this.userId.getId());
-    } catch (error) {
-      throw error;
-    }
   }
 
   async approveDoctor(doctorId) {
-    try {
       await this.verifyAdmin();
       return await this.adminRepository.approveDoctor(doctorId, this.userId.getId());
-    } catch (error) {
-      throw error;
-    }
   }
 
   async rejectDoctor(doctorId) {
-    try {
       await this.verifyAdmin();
       return await this.adminRepository.rejectDoctor(doctorId, this.userId.getId());
-    } catch (error) {
-      throw error;
-    }
   }
 
   async viewPatients() {
-    try {
       await this.verifyAdmin();
       return await this.adminRepository.fetchPendingPatients(this.userId.getId());
-    } catch (error) {
-      throw error;
-    }
   }
 
   async approvePatient(patientId) {
-    try {
       await this.verifyAdmin();
       return await this.adminRepository.approvePatient(patientId, this.userId.getId());
-    } catch (error) {
-      throw error;
-    }
   }
 
   async rejectPatient(patientId) {
-    try {
       await this.verifyAdmin();
       return await this.adminRepository.rejectPatient(patientId, this.userId.getId());
-    } catch (error) {
-      throw error;
-    }
   }
 
   async viewHealthOfficers() {
-    try {
       await this.verifyAdmin();
       return await this.adminRepository.fetchPendingHealthOfficers(this.userId.getId());
-    } catch (error) {
-      throw error;
-    }
   }
 
   async approveHealthOfficer(officerId) {
-    try {
       await this.verifyAdmin();
       return await this.adminRepository.approveHealthOfficer(officerId, this.userId.getId());
-    } catch (error) {
-      throw error;
-    }
   }
 
   async rejectHealthOfficer(officerId) {
-    try {
       await this.verifyAdmin();
       return await this.adminRepository.rejectHealthOfficer(officerId, this.userId.getId());
-    } catch (error) {
-      throw error;
-    }
   }
 
   async viewImmigrationOfficers() {
-    try {
       await this.verifyAdmin();
       return await this.adminRepository.fetchPendingImmigrationOfficers(this.userId.getId());
-    } catch (error) {
-      throw error;
-    }
   }
 
   async approveImmigrationOfficer(officerId) {
-    try {
       await this.verifyAdmin();
       return await this.adminRepository.approveImmigrationOfficer(officerId, this.userId.getId());
-    } catch (error) {
-      throw error;
-    }
   }
 
   async rejectImmigrationOfficer(officerId) {
-    try {
       await this.verifyAdmin();
       return await this.adminRepository.rejectImmigrationOfficer(officerId, this.userId.getId());
-    } catch (error) {
-      throw error;
-    }
   }
 
   assignPatient(patient, doctor) {
