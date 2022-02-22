85720d513bda148be3e5b6ef1d17d90eefde540f
Zain-MKhan <zmkhan025@gmail.com>
2022-02-21 23:11:53 -0500

    successfully-displayed-patient-info

--- frontend/src/components/Dashboards/DoctorDashboard/DoctorDashboard.js
+++ frontend/src/components/Dashboards/DoctorDashboard/DoctorDashboard.js
@@ -10,50 +10,15 @@ import NotificationBox from "../../NotificationBox/NotificationBox";
 import axios from "axios";
 import { useAuth } from "../../Authentication/FirebaseAuth/FirebaseAuth";
 import { useState } from "react";
+import MonitorPatients from "../../Tabs/MonitorPatients";
 
 function DoctorDashboard(props) {
-
-  //the following const's are temporary to test out the UI without the backend
-  //creating three Patient objects, then populating an array with these patients
-  const Patient = function(pName, pStatus, pDob, pEmail, pPhoneNumber)
-  {
-      const name = pName;
-      const status = pStatus;
-      const dob = pDob;
-      const email = pEmail;
-      const phoneNumber = pPhoneNumber;
-
-      return {name, status, dob, email, phoneNumber};
-  };
-  const patientOne = Patient("First Patient", "POSITIVE", "02/05/1999", "Patient1@gmail.com", "514-514-5145");
-  const patientTwo = Patient("Second Patient", "PENDING", "14/02/2014", "iAmPatient2@gmail.com", "438-438-4384");
-  const patientThree = Patient("Third Patient", "NEGATIVE", "17/08/1984", "patientTHREE@gmail.com", "514-438-5145");
-  // const patients = [patientOne, patientTwo, patientThree];
-
-
-  let {currentUser} = useAuth();
-
-  const [patientList, setPatientList] = useState([]);
-
-  const getPatientArray = async () => {
-     axios.get( `doctor/${currentUser.user.uid}/patientArray` )
-    //axios.get('/:doctor_id/patientArray')
-    .then((response) => {
-      setPatientList(response.data.data);
-      console.log(patientList);
-    })
-    .catch((error) => {
-      console.log(error.response);
-    })
-  }
-
-
   return (
     <div>
       <Navbar />
       <div className={styles["main-container"]}>
         <div className={styles["tabs-box"]}>
-          <Tabs className="tabStyle" defaultActiveKey="home">
+          <Tabs className="tabStyle" defaultActiveKey="home" unmountOnExit={true} mountOnEnter={true}>
             <Tab eventKey="home" title="Home">
               <div className={styles["tab-page"]}>
                 <Home />
@@ -61,32 +26,7 @@ function DoctorDashboard(props) {
             </Tab>
             <Tab eventKey="monitor-patient" title="Monitor Patients" >
             <div className={styles["tab-page"]}>
-              <div>
-                {patientList.map(patient =>
-                <div className={styles["patientInfoCard"]}>
-                  <div>
-                    <h6>Name:</h6>
-                    <p>{patient.name}</p>
-                  </div>
-                  <div>
-                    <h6>Status:</h6>
-                    <p>{patient.status}</p> 
-                  </div>
-                  <div>
-                    <h6>Date of Birth:</h6>
-                    <p>{patient.dob}</p> 
-                  </div>
-                  <div>
-                    <h6>Email:</h6>
-                    <p>{patient.email}</p> 
-                  </div>
-                  <div>
-                    <h6>Phone Number:</h6>
-                    <p>{patient.phoneNumber}</p>
-                  </div>
-                </div>
-                )}
-              </div>
+              <MonitorPatients />
             </div>
             </Tab>
             <Tab eventKey="contact" title="Contact"></Tab>
---
+++ frontend/src/components/Tabs/MonitorPatients.js
@@ -0,0 +1,88 @@
+import photo1 from "../../assets/HomePhotos/doctor_sitting.jpg";
+import photo3 from "../../assets/HomePhotos/closed.jpg";
+import { Accordion } from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { useAuth } from "../Authentication/FirebaseAuth/FirebaseAuth";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./MonitorPatients.module.css";
+import "./CommonPageStyling.css";
+import axios from "axios";
+import { ListGroup } from "react-bootstrap";
+
+function MonitorPatients(props) {
+
+    // //the following 5 const's are to test the ui without the backend:
+    // const Patient = function(pName, pStatus, pDob, pEmail, pPhoneNumber)
+    // {
+    //     const name = pName;
+    //     const status = pStatus;
+    //     const dob = pDob;
+    //     const email = pEmail;
+    //     const phoneNumber = pPhoneNumber;
+  
+    //     return {name, status, dob, email, phoneNumber};
+    // };
+    // const patientOne = Patient("First Patient", "POSITIVE", "02/05/1999", "Patient1@gmail.com", "514-514-5145");
+    // const patientTwo = Patient("Second Patient", "PENDING", "14/02/2014", "iAmPatient2@gmail.com", "438-438-4384");
+    // const patientThree = Patient("Third Patient", "NEGATIVE", "17/08/1984", "patientTHREE@gmail.com", "514-438-5145");
+    // const patients = [patientOne, patientTwo, patientThree];
+
+    const [patientList, setPatientList] = useState([]);
+    
+    const getPatientArray = () => {
+        axios.get( `doctor/${currentUser.user.uid}/patientArray` )
+        .then((response) => {
+        setPatientList(response.data.data);
+        console.log(patientList);
+        console.log(response);
+        })
+        .catch((error) => {
+        console.log(error.response);
+        })
+    }
+    
+    let {currentUser} = useAuth();
+
+    const renderPatientList = () => {
+        return <div>
+          {patientList.map(patient =>
+            <div className={styles["patientInfoCard"]}>
+                <div>
+                    <h6>Name:</h6>
+                    <p>{patient.name}</p>
+                </div>
+                <div>
+                    <h6>Status:</h6>
+                    <p>{patient.status ? patient.status : 'null'}</p> 
+                </div>
+                <div>
+                    <h6>Date of Birth:</h6>
+                    <p>{patient.dob ? patient.dob : 'null'}</p> 
+                </div>
+                <div>
+                    <h6>Email:</h6>
+                    <p>{patient.email}</p> 
+                </div>
+                <div>
+                    <h6>Phone Number:</h6>
+                    <p>{patient.phoneNumber ? patient.phoneNumber.phoneNumber || patient.phoneNumber : 'null'}</p>
+                </div>
+            </div>
+            )}
+        </div>
+    }
+
+    useEffect(() => {
+        getPatientArray();
+    }, [patientList.length]);
+
+  return (
+    <div>
+        {renderPatientList()}
+    </div>
+  );
+}
+
+export default MonitorPatients;
