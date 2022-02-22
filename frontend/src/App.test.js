2794a3368ea3031f1a940c646c1506819a1ee6ac
p-carrier <p-carrier@users.noreply.github.com>
2022-02-22 13:10:00 -0500

    remove unused element

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import {BrowserRouter, MemoryRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 
 import { AuthContext } from './components/Authentication/FirebaseAuth/FirebaseAuth';
 
