5ca4e94f164f5675c521c29a6012f5b7c88cebed
p-carrier <p-carrier@users.noreply.github.com>
2022-02-22 13:08:47 -0500

    refactor app.js and index.js for easy testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,83 +7,79 @@ import AdminDashboard from "./components/Dashboards/Admin Dashboard/AdminDashboa
 import HODashboard from "./components/Dashboards/Health Official Dashboard/HODashboard";
 import IODashboard from "./components/Dashboards/Immigration Officer Dashboard/IODashboard";
 import GeneralDashboard from "./components/Dashboards/GeneralDashboard/GeneralDashboard";
-import FirebaseAuthProvider from "./components/Authentication/FirebaseAuth/FirebaseAuth";
+
 import "./components/Authentication/FirebaseAuth/FirebaseConfig";
 import PrivateRoute from "./components/RouteHandler/PrivateRoute";
 import UserProfile from "./components/UserProfile/UserProfile";
 
 function App() {
   return (
-    <BrowserRouter>
-      <FirebaseAuthProvider>
-        <div className="App">
-          <main>
-            <Routes>
-              <Route path="/" element={<Login />} />
-              <Route path="/signup" element={<SignUp />} />
-              <Route
-                path="/patient-dashboard"
-                element={
-                  <PrivateRoute requestedRoute={"/patient-dashboard"}>
-                    <PatientDashboard />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/doctor-dashboard"
-                element={
-                  <PrivateRoute requestedRoute={"/doctor-dashboard"}>
-                    <DoctorDashboard />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/admin-dashboard"
-                element={
-                  <PrivateRoute requestedRoute={"/admin-dashboard"}>
-                    <AdminDashboard />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/health-official-dashboard"
-                element={
-                  <PrivateRoute requestedRoute={"/health-official-dashboard"}>
-                    <HODashboard />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/immigration-officer-dashboard"
-                element={
-                  <PrivateRoute
-                    requestedRoute={"/immigration-officer-dashboard"}
-                  >
-                    <IODashboard />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/general-dashboard"
-                element={
-                  <PrivateRoute requestedRoute={"/general-dashboard"}>
-                    <GeneralDashboard />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/user-profile"
-                element={
-                  <PrivateRoute requestedRoute={"/user-profile"}>
-                    <UserProfile />
-                  </PrivateRoute>
-                }
-              />
-            </Routes>
-          </main>
-        </div>
-      </FirebaseAuthProvider>
-    </BrowserRouter>
+    <div className="App">
+      <main>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route
+            path="/patient-dashboard"
+            element={
+              <PrivateRoute requestedRoute={"/patient-dashboard"}>
+                <PatientDashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/doctor-dashboard"
+            element={
+              <PrivateRoute requestedRoute={"/doctor-dashboard"}>
+                <DoctorDashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/admin-dashboard"
+            element={
+              <PrivateRoute requestedRoute={"/admin-dashboard"}>
+                <AdminDashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/health-official-dashboard"
+            element={
+              <PrivateRoute requestedRoute={"/health-official-dashboard"}>
+                <HODashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/immigration-officer-dashboard"
+            element={
+              <PrivateRoute
+                requestedRoute={"/immigration-officer-dashboard"}
+              >
+                <IODashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/general-dashboard"
+            element={
+              <PrivateRoute requestedRoute={"/general-dashboard"}>
+                <GeneralDashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/user-profile"
+            element={
+              <PrivateRoute requestedRoute={"/user-profile"}>
+                <UserProfile />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import {BrowserRouter, MemoryRouter} from 'react-router-dom'
+
+import { AuthContext } from './components/Authentication/FirebaseAuth/FirebaseAuth';
 
 test("renders learn react link", () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <AuthContext.Provider value={{
+        currentUser: {
+          user: 'john@email.com',
+        },
+        logout: () => {},
+      }}>
+        <App />
+      </AuthContext.Provider>
+    </BrowserRouter>);
+
   const linkElement = screen.getByText(/CoviCare/i);
   expect(linkElement).toBeInTheDocument();
 });
