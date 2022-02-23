cfe14d1df3bab4091b83a01677f344f4cd3e2282
p-carrier <p-carrier@users.noreply.github.com>
2022-02-23 10:27:53 -0500

    fix error by closing server

--- routes/notification.test.js
+++ routes/notification.test.js
@@ -2,11 +2,16 @@ const request = require('supertest');
 const MongoClient = require('mongodb').MongoClient;
 const {Notification} = require('../domain/notification');
 const app = require('../app');
+const server = require('../app').server;
 
 jest.mock('../domain/notification');
 jest.mock('mongodb');
 
 describe('integration test notificationRoutes - connection to notification domain', () => {
+  afterAll(() => {
+    server.close();
+  });
+  
   describe('test GET /notification/:notificationId/view', () => {
     jest.spyOn(Notification.prototype, 'viewNotification').mockImplementation(() => {});
     jest.spyOn(MongoClient.prototype, 'connect').mockImplementation(() => {});
