f56c90199caa8849945864e65e16bca6d423ed96
p-carrier <p-carrier@users.noreply.github.com>
2022-02-22 23:20:17 -0500

    basic lint to merge master

--- domain/notification.js
+++ domain/notification.js
@@ -1,28 +1,28 @@
 class Notification {
-    constructor(notificationId, notificationRepository) {
-        this.notificationId = notificationId;
-        this.notificationRepository = notificationRepository;
-    }
+  constructor(notificationId, notificationRepository) {
+    this.notificationId = notificationId;
+    this.notificationRepository = notificationRepository;
+  }
 
-    async createNotification(notificationData) {
-        return await this.notificationRepository.addNewNotification(notificationData);
-    }
+  async createNotification(notificationData) {
+    return await this.notificationRepository.addNewNotification(notificationData);
+  }
 
-    async createManyNotifications(notificationData) {
-        return await this.notificationRepository.addManyNotifications(notificationData);
-    }
+  async createManyNotifications(notificationData) {
+    return await this.notificationRepository.addManyNotifications(notificationData);
+  }
 
-    async removeNotification() {
-        return await this.notificationRepository.deleteNotification(this.notificationId);
-    }
+  async removeNotification() {
+    return await this.notificationRepository.deleteNotification(this.notificationId);
+  }
 
-    async viewNotification() {
-        return await this.notificationRepository.getNotification(this.notificationId);
-    }
+  async viewNotification() {
+    return await this.notificationRepository.getNotification(this.notificationId);
+  }
 
-    getNotificationId() {
-        return this.notificationId._id;
-    }
+  getNotificationId() {
+    return this.notificationId._id;
+  }
 }
 
 module.exports.Notification = Notification;
