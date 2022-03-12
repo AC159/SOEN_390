1e25922166335efefb46fa87b5fc956109fd4277
j-5844 <juhirao5844@hotmail.com>
2022-03-12 17:50:54 -0500

    Doctor can see scheduled and book  appointments #4
    bug: patient name does not show up in scheduled appointments
    bug: time slots not blocked out when already taken

--- frontend/src/components/Tabs/DoctorTabs/Appointment.js
+++ frontend/src/components/Tabs/DoctorTabs/Appointment.js
@@ -15,44 +15,120 @@ import setMinutes from "date-fns/setMinutes";
 function Appointment(props) {
      
     let {currentUser} = useAuth();
-    const [value, onChange] = useState(new Date());
     const [patientList, setPatientList] = useState([]);
+    const [appointments, setAppointments] = useState([]);
+
+    let [patientId, setPatientId] = useState('');
+    let [patientName, setPatientName] = useState('');
+    let [dateAndTime, setDateAndTime] = useState(new Date);
+    let [title, setTitle] = useState('')
+    let [information, setInformation] = useState('');
     
     const getPatientArray = async () => {
         try {
             const response = await axios.get(`doctor/${currentUser.user.uid}/patientArray`);
             setPatientList(response.data.data);
+            console.log('Patient Array received!');
+            console.log(patientList);
         } catch (error) {
             console.log(error);
         }
-    }
-
-    const renderPatientList = () => {
-        let optionPatient = patientList.map((patient) =>
-                <option data-testid="patient-name">{patient.name}</option> 
-            );
-
-        return (
-            <div>
-                <select>
-                    {optionPatient}
-                </select>
-            </div>
-        )
     }
 
     useEffect(() => {
         getPatientArray();
     }, [patientList.length]);
 
+    const submitAppointment = async () => {
+        try {
+            const userAttributes = {
+                doctorId: currentUser.user.uid,
+                patientId: patientId,
+                patientName: patientName,
+                dateAndTime: dateAndTime,
+                title: title,
+                information: information
+            };
+            axios.post(`doctor/${currentUser.user.uid}/appointment`, userAttributes)
+                .then(function (response) {
+                    console.log(response);
+                })
+        } catch (error) {
+            console.log('Submit error: ', error);
+        }
+    }
+
+    const renderPatientList = () => {
+        let optionPatient = patientList.map((patient) =>
+                <option data-testid="patient-name" value={patient.uid}>{patient.name}</option> 
+            );
+
+        return (
+            <div>
+                <select onChange={(event) => {setPatientId(event.target.value);}}>
+                    {optionPatient}
+                </select>
+            </div>
+        )
+    }
+
+    const getAppointments = async () => {
+        try {
+            const response = await axios.get(`doctor/${currentUser.user.uid}/appointments`);
+            console.log('Appointments received!');
+            console.log(response.data.data.length);
+            setAppointments(response.data.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getAppointments();
+    }, [appointments.length]);
+
+
+    const renderAppointments = () => {
+        return <div className={styles["noAppointmentsMessage"]}>
+        {appointments.length === 0 ? <div>You have no appointments at this time.</div> : 
+        
+        appointments.map((appointments, index) =>
+            <div key = {index} className={styles["appointmentList"]}>
+                <div>
+                    <h6>Patient:</h6>
+                    <p data-testid="appointment-name">{appointments.patientName}</p>
+                </div>
+                <div>
+                    <h6>Title:</h6>
+                    <p data-testid="appointment-title">{appointments.title}</p>
+                </div>
+                <div>
+                    <h6>Info:</h6>
+                    <p data-testid="appointment-info">{appointments.information}</p>
+                </div>
+                <div>
+                    <h6>Date and Time:</h6>
+                    <p data-testid="appointment-dateTime">{appointments.dateAndTime}</p>
+                </div>
+            </div>
+            )}
+        </div>
+    }
+
     return (
         <div>
+            <h4>Scheduled Appointments</h4>
+            {renderAppointments()}
+            <br></br>
+            <h4>Add Appointment</h4>
             <form>
+                <p>Select Patient:</p>
                 {renderPatientList()}
                 <div>
+                    <p>Select Date & Time: </p>
                     <DatePicker 
-                        selected={value} 
-                        onChange={onChange} 
+                        selected={dateAndTime} 
+                        onChange={(dateAndTime) => setDateAndTime(dateAndTime)} 
                         inline
                         showTimeSelect
                         timeIntervals={30}
@@ -61,14 +137,16 @@ function Appointment(props) {
                         timeCaption="Time"
                         dateFormat="h:mm aa"
                         popperPlacement="top-end"
-                        
                     />
                 </div>
-                <Button>
+                <p>Meeting Title:</p>
+                <textarea rows="1" cols="50" onChange={(event) => setTitle(event.target.value)}/>
+                <p>More Info:</p>
+                <textarea rows="4" cols="50" onChange={(event) => setInformation(event.target.value)}/>
+                <Button onClick={event => { submitAppointment(); }}>
                     Submit
                 </Button>
             </form>
-
         </div>
     );
 }
