f5286a6e21a0d590313eaf0599f562fb82753b7a
ani-6292 <anita6292@hotmail.com>
2022-03-15 01:16:16 -0400

    Appointments completed #4
    Fixed: Patient name does not show up in scheduled appointments.
    Fixed: Time slots not blocked out when already taken.
    Finalized CSS.
    
    TODO: frontend testing

--- frontend/src/components/Tabs/DoctorTabs/Appointment.js
+++ frontend/src/components/Tabs/DoctorTabs/Appointment.js
@@ -4,12 +4,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./Appointment.module.css";
 import "../CommonPageStyling.css";
 import axios from "axios";
-import {Button} from "react-bootstrap";
+import {Button, Container, Row, Col} from "react-bootstrap";
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import setHours from "date-fns/setHours";
 import setMinutes from "date-fns/setMinutes";
-
+import moment from "moment";
 
 
 function Appointment(props) {
@@ -23,13 +23,14 @@ function Appointment(props) {
     let [dateAndTime, setDateAndTime] = useState(new Date);
     let [title, setTitle] = useState('')
     let [information, setInformation] = useState('');
+    let dateArr = [];
+    let arrExcludedTimes = [];
     
+    //Get array of patients assigned to specific doctor.
     const getPatientArray = async () => {
         try {
             const response = await axios.get(`doctor/${currentUser.user.uid}/patientArray`);
             setPatientList(response.data.data);
-            console.log('Patient Array received!');
-            console.log(patientList);
         } catch (error) {
             console.log(error);
         }
@@ -37,15 +38,49 @@ function Appointment(props) {
 
     useEffect(() => {
         getPatientArray();
-    }, [patientList.length]);
+    }, []);
 
+    //Returns patient ID based on selected patient name.
+    function id(pName){
+        let patientArr = [];
+        let uName = pName;
+        for(var i of patientList)
+        {
+            patientArr.push(i);
+        }
+
+        var index = patientArr.findIndex(item => item.name === uName);
+        let patient = patientArr[index];
+        let pId = patient["uid"];
+
+        return(
+            pId
+        )
+    }
+
+    //Dropdown including all patients assigned to a doctor.
+    const renderPatientList = () => {
+        let optionPatient = patientList.map((patient, index) =>
+                <option data-testid="patient-name" key={index}>{patient.name}</option> 
+            );
+
+        return (
+            <div>
+                <select onChange={(event) => {setPatientId(id(event.target.value)); setPatientName(event.target.value);}}>
+                    {optionPatient}
+                </select>
+            </div>
+        )
+    }
+
+    //Submit appointment information into database.
     const submitAppointment = async () => {
         try {
             const userAttributes = {
                 doctorId: currentUser.user.uid,
                 patientId: patientId,
                 patientName: patientName,
-                dateAndTime: dateAndTime,
+                dateAndTime: dateAndTime.getTime(),
                 title: title,
                 information: information
             };
@@ -58,25 +93,10 @@ function Appointment(props) {
         }
     }
 
-    const renderPatientList = () => {
-        let optionPatient = patientList.map((patient) =>
-                <option data-testid="patient-name" value={patient.uid}>{patient.name}</option> 
-            );
-
-        return (
-            <div>
-                <select onChange={(event) => {setPatientId(event.target.value);}}>
-                    {optionPatient}
-                </select>
-            </div>
-        )
-    }
-
+    //Retrieve appointment information from database.
     const getAppointments = async () => {
         try {
             const response = await axios.get(`doctor/${currentUser.user.uid}/appointments`);
-            console.log('Appointments received!');
-            console.log(response.data.data.length);
             setAppointments(response.data.data);
         } catch (error) {
             console.log(error);
@@ -87,7 +107,57 @@ function Appointment(props) {
         getAppointments();
     }, [appointments.length]);
 
+    //Change timestamp into readable date and time.
+    //Also get appointment times already booked to exclude them from possible future bookings.
+    function showTimeStamp(timeStamp) {
+        const fullDate = new Intl.DateTimeFormat('en-US', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit'
+        }).format(timeStamp)
 
+        var year = parseInt(fullDate.slice(6,10));
+        var month = parseInt(fullDate.slice(0,2))-1;
+        var day = parseInt(fullDate.slice(3,5));
+        var hour = parseInt(fullDate.slice(12,14));
+        var minute = parseInt(fullDate.slice(15,17));
+        var ampm = fullDate.slice(18);
+
+        if (ampm === 'PM')
+        {
+            hour = hour + 12;
+        }
+
+        var newDate = new Date(year, month, day, hour, minute);
+        dateArr.push(newDate);
+        
+        let arrSpecificDates = [];
+        for (let i = 0; i < dateArr.length; i++) {
+            if (
+                moment(dateAndTime, moment.ISO_8601).format("YYYY/MM/DD") ===
+                moment(dateArr[i], moment.ISO_8601).format("YYYY/MM/DD")
+            ) {
+                arrSpecificDates.push(moment(dateArr[i], moment.ISO_8601).toObject());
+            }
+        }
+
+        for (let i = 0; i < arrSpecificDates.length; i++) {
+            arrExcludedTimes.push(
+                setHours(
+                setMinutes(new Date(), arrSpecificDates[i].minutes),
+                arrSpecificDates[i].hours
+                )
+            );
+        }
+
+        return(
+            fullDate
+        );
+    }
+
+    //Show list of existing appointments.
     const renderAppointments = () => {
         return <div className={styles["noAppointmentsMessage"]}>
         {appointments.length === 0 ? <div>You have no appointments at this time.</div> : 
@@ -108,7 +178,7 @@ function Appointment(props) {
                 </div>
                 <div>
                     <h6>Date and Time:</h6>
-                    <p data-testid="appointment-dateTime">{appointments.dateAndTime}</p>
+                    <p data-testid="appointment-dateTime">{showTimeStamp(appointments.dateAndTime)}</p>
                 </div>
             </div>
             )}
@@ -117,37 +187,60 @@ function Appointment(props) {
 
     return (
         <div>
-            <h4>Scheduled Appointments</h4>
+            <h4 className={styles["scheduleAppointment"]}>Scheduled Appointments</h4>
             {renderAppointments()}
             <br></br>
-            <h4>Add Appointment</h4>
-            <form>
-                <p>Select Patient:</p>
-                {renderPatientList()}
-                <div>
-                    <p>Select Date & Time: </p>
-                    <DatePicker 
-                        selected={dateAndTime} 
-                        onChange={(dateAndTime) => setDateAndTime(dateAndTime)} 
-                        inline
-                        showTimeSelect
-                        timeIntervals={30}
-                        minTime={setHours(setMinutes(new Date(), 0), 7)}
-                        maxTime={setHours(setMinutes(new Date(), 0), 20)}
-                        timeCaption="Time"
-                        dateFormat="h:mm aa"
-                        popperPlacement="top-end"
-                    />
+            
+            <div className={styles["addAppointment"]}>
+                <h4>Add Appointment</h4>
+                <div className={styles["form-group"]}>
+                    <form className={styles["bookAppointmentForm"]}> 
+                        <div className={styles["selectPatient"]}>
+                            <p>Select Patient:</p>
+                            {renderPatientList()}
+                        </div>
+                        <Container>
+                            <Row>
+                                <Col sm={4}>
+                                    <div className={styles["datePicker"]}>
+                                        <p>Select Date & Time: </p>
+                                        <DatePicker 
+                                            selected={dateAndTime} 
+                                            excludeTimes={arrExcludedTimes}
+                                            onChange={(dateAndTime) => setDateAndTime(dateAndTime)} 
+                                            inline
+                                            showTimeSelect
+                                            timeIntervals={30}
+                                            minTime={setHours(setMinutes(new Date(), 0), 7)}
+                                            maxTime={setHours(setMinutes(new Date(), 0), 20)}
+                                            timeCaption="Time"
+                                            dateFormat="h:mm aa"
+                                            popperPlacement="top-end"
+                                        />
+                                    </div>
+                                </Col>
+                                <Col sm={8}>
+                                    <div className={styles["appointmentInfo"]}>
+                                        <p>Meeting Title:</p>
+                                        <textarea onChange={(event) => setTitle(event.target.value)}/>
+                                        <p>More Info:</p>
+                                        <textarea className={styles["moreInfo"]} onChange={(event) => setInformation(event.target.value)}/>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </Container>
+                        <br></br>
+
+                        <div class="col-md-12 text-center">
+                            <Button class="btn btn-outline-dark justify-content-center" variant="secondary" onClick={event => { submitAppointment(); }}>
+                                Book Appointment
+                            </Button>
+                        </div>
+                    </form>
                 </div>
-                <p>Meeting Title:</p>
-                <textarea rows="1" cols="50" onChange={(event) => setTitle(event.target.value)}/>
-                <p>More Info:</p>
-                <textarea rows="4" cols="50" onChange={(event) => setInformation(event.target.value)}/>
-                <Button onClick={event => { submitAppointment(); }}>
-                    Submit
-                </Button>
-            </form>
+            </div>
         </div>
     );
 }
+
 export default Appointment;
