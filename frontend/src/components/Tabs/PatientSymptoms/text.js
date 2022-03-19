7fc2ca6cf813718861551ac203cd216ac26d7880
p-carrier <p-carrier@users.noreply.github.com>
2022-03-19 11:32:56 -0400

    refactor Covid File: simplify form & initialize form for new form

---
+++ frontend/src/components/Tabs/PatientSymptoms/text.js
@@ -0,0 +1,16 @@
+const symptoms = {
+  fever: "Fever or feeling feverish (such as chills, sweating)",
+  breathing:
+    "Mild or moderate difficulty breathing (breathing slightly faster than normal, feeling like you can’t inhale or exhale, or wheezing, especially during exhaling or breathing out)",
+  throat: "Sore throat",
+  muscle: "Muscle aches or body aches",
+  fatigue: "Unusual fatigue",
+  headache: "Headache",
+  taste: "New loss of taste or smell",
+  congestion: "Congestion or runny nose",
+  nausea: "Nausea or vomiting",
+  diarrhea: "Diarrhea",
+  other: "Other symptoms",
+};
+
+export default symptoms;
