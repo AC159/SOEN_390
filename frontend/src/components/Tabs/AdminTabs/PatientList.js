00e2b30598698bb7cfe547f2a05206793397433f
mth-1012 <mth.1012.ta@gmail.com>
2022-03-22 12:25:19 -0400

    Added a search box (by name) for PatientList of admin, healthOfficial and immigrationOfficial #87

--- frontend/src/components/Tabs/AdminTabs/PatientList.js
+++ frontend/src/components/Tabs/AdminTabs/PatientList.js
@@ -2,63 +2,85 @@ import React, {useEffect, useState} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import PatientBox from "../../GeneralComponents/PatientBox";
 import styles from "./PatientList.module.css";
-import { useAuth } from "../../Authentication/FirebaseAuth/FirebaseAuth";
+import {useAuth} from "../../Authentication/FirebaseAuth/FirebaseAuth";
 import axios from "axios";
-import { Accordion } from "react-bootstrap";
+import {Accordion} from "react-bootstrap";
 
 function PatientList(props) {
 
   let {currentUser} = useAuth();
   const [patientList, setPatientList] = useState([]);
- 
+  const [searchTerm, setSearchTerm] = useState('')
 
-  useEffect(() => {fetchListOfPatients(); console.log(localStorage.getItem("userType"))}, [patientList.length]);
+  useEffect(() => {
+    fetchListOfPatients();
+    console.log(localStorage.getItem("userType"))
+  }, [patientList.length]);
 
   const fetchListOfPatients = async () => {
     try {
-        let routeType;
-        switch(localStorage.getItem("userType"))
-        {
-          case "administrator": routeType="admin"; break;
-          case "immigrationOfficial": routeType="immigration-official"; break;
-          case "healthOfficial": routeType="health-official"; break;
+      let routeType;
+      switch (localStorage.getItem("userType")) {
+        case "administrator":
+          routeType = "admin";
+          break;
+        case "immigrationOfficial":
+          routeType = "immigration-official";
+          break;
+        case "healthOfficial":
+          routeType = "health-official";
+          break;
 
-          default: routeType="admin";
+        default:
+          routeType = "admin";
 
-        }
-        const response = await axios.get(`${routeType}/${currentUser.user.uid}/patients`);
-        setPatientList(response.data.data);
-        console.log(response.data.data)
+      }
+      const response = await axios.get(`${routeType}/${currentUser.user.uid}/patients`);
+      setPatientList(response.data.data);
+      console.log(response.data.data)
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
   }
 
   function renderPatientList() {
     return <Accordion>
-      {patientList.map((patient, index) => 
-      <PatientBox
-      key={index}
-      eventKey={index}
-      patient={patient}
-      doctorName={patient.patientInfo.doctor}
-      currentUser={currentUser}
-      userType={localStorage.getItem("userType")}
-      className={styles["patient-box"]}
-      />)}
+      {patientList
+        .filter((item) => {
+            if (searchTerm === "")
+              return item;
+            if (item.name.toLowerCase().includes(searchTerm.toLowerCase()))
+              return item;
+          })
+        .map((patient, index) =>
+        <PatientBox
+          key={index}
+          eventKey={index}
+          patient={patient}
+          doctorName={patient.patientInfo.doctor}
+          currentUser={currentUser}
+          userType={localStorage.getItem("userType")}
+          className={styles["patient-box"]}
+        />)}
     </Accordion>;
   }
 
   return (
     <div className={styles["role-outer-container"]}>
       <div className={styles["todays-new-title"]}>Patient List</div>
-      <hr />
+      <hr/>
+      <input
+        type="text" placeholder="Search patient by name..."
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      />
       <div>
         {renderPatientList()}
       </div>
-        
+
       <div className={styles["request-container"]}>
-        
+
       </div>
     </div>
   );
