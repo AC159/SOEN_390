eef8900e2c1a9554378b99c59a9f5fc604b52c16
p-carrier <p-carrier@users.noreply.github.com>
2022-03-24 10:59:48 -0400

    file formatting

--- frontend/src/components/Tabs/PatientSymptoms/ContactTrace.js
+++ frontend/src/components/Tabs/PatientSymptoms/ContactTrace.js
@@ -1,31 +1,25 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import moment from "moment";
-import Modal from "react-bootstrap/Modal";
-import { Accordion, Button } from "react-bootstrap";
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import moment from 'moment';
+import Modal from 'react-bootstrap/Modal';
+import {Accordion, Button} from 'react-bootstrap';
 
-import { useAuth } from "../../Authentication/FirebaseAuth/FirebaseAuth";
-import useInputField from "../../../hook/useInputField";
-import styles from "./ContactTrace.module.css";
+import {useAuth} from '../../Authentication/FirebaseAuth/FirebaseAuth';
+import useInputField from '../../../hook/useInputField';
+import styles from './ContactTrace.module.css';
 
-const initialEmailsState = [{ email: "" }];
-const newEmail = { email: "" };
+const initialEmailsState = [{email: ''}];
+const newEmail = {email: ''};
 
 function ContactTrace(props) {
-  let { currentUser } = useAuth();
-  let [locationDescription, setLocationDescription] = useState("");
-  let [date, setDate] = useState("");
+  let {currentUser} = useAuth();
+  let [locationDescription, setLocationDescription] = useState('');
+  let [date, setDate] = useState('');
   let [showModal, setShowModal] = useState(false);
   let [patientTracing, setTracing] = useState();
-  let [timeStamp, setTimeStamp] = useState("");
+  let [timeStamp, setTimeStamp] = useState('');
   let [update, setUpdate] = useState(false);
-  const [
-    emails,
-    setEmails,
-    handleEmailFieldChange,
-    handleAddEmailField,
-    handleDeleteEmailField,
-  ] = useInputField(initialEmailsState, newEmail);
+  const [emails, setEmails, handleEmailFieldChange, handleAddEmailField, handleDeleteEmailField] = useInputField(initialEmailsState, newEmail);
 
   const submitContactTracingForm = async () => {
     try {
@@ -37,13 +31,10 @@ function ContactTrace(props) {
         date: date,
         locationDescription: locationDescription,
       };
-      const response = await axios.post(
-        `/patient/submit-contact-tracing`,
-        userAttributes
-      );
+      const response = await axios.post(`/patient/submit-contact-tracing`, userAttributes);
       console.log(response);
     } catch (error) {
-      console.log("Submit error: ", error);
+      console.log('Submit error: ', error);
     }
   };
 
@@ -57,10 +48,7 @@ function ContactTrace(props) {
     };
     console.log(userAttributes);
     axios
-      .post(
-        `/patient/update-contact-tracing/${currentUser.user.uid}&timeStamp=${timeStamp}`,
-        userAttributes
-      )
+      .post(`/patient/update-contact-tracing/${currentUser.user.uid}&timeStamp=${timeStamp}`, userAttributes)
       .then(function (response) {
         console.log(response);
       })
@@ -71,13 +59,11 @@ function ContactTrace(props) {
 
   const fetchPatientContactTracing = async () => {
     try {
-      const response = await axios.get(
-        `/patient/get-contact-tracing/${currentUser.user.uid}`
-      );
+      const response = await axios.get(`/patient/get-contact-tracing/${currentUser.user.uid}`);
       console.log(response);
       setTracing(response.data);
     } catch (error) {
-      console.log("Unable to fetch patient status forms: ", error);
+      console.log('Unable to fetch patient status forms: ', error);
     }
   };
 
@@ -107,8 +93,8 @@ function ContactTrace(props) {
   return (
     <div>
       <Button
-        variant="info"
-        className={styles["submit-button-trace"]}
+        variant='info'
+        className={styles['submit-button-trace']}
         onClick={() => {
           openModal();
           setUpdate(false);
@@ -118,33 +104,23 @@ function ContactTrace(props) {
         New Contact Tracing from
       </Button>
       {patientTracing ? (
-        <Accordion defaultActiveKey="0">
+        <Accordion defaultActiveKey='0'>
           {patientTracing.map((element, index) => {
             let date = new Date(element.timeStamp);
             return (
               <Accordion.Item eventKey={index} key={index}>
-                <Accordion.Header>
-                  Created on{" "}
-                  {moment(date).format("dddd, MMMM Do YYYY, h:mm:ss a")}
-                </Accordion.Header>
+                <Accordion.Header>Created on {moment(date).format('dddd, MMMM Do YYYY, h:mm:ss a')}</Accordion.Header>
                 <Accordion.Body>
                   {Object.entries(element).map(([key, value], index) => {
-                    if (key === "timeStamp") {
+                    if (key === 'timeStamp') {
                       // setTimeStamp({value})
                     }
-                    if (
-                      !value ||
-                      key === "_id" ||
-                      key === "patientUid" ||
-                      key === "timeStamp" ||
-                      value.length === 0
-                    )
-                      return null;
+                    if (!value || key === '_id' || key === 'patientUid' || key === 'timeStamp' || value.length === 0) return null;
 
                     if (Array.isArray(value)) {
                       return (
                         <div key={index}>
-                          <strong>{key}</strong>:{" "}
+                          <strong>{key}</strong>:{' '}
                           <ul>
                             {value.map((element, i) => (
                               <li key={i}>{element}</li>
@@ -162,7 +138,7 @@ function ContactTrace(props) {
                 </Accordion.Body>
               </Accordion.Item>
             );
-          })}{" "}
+          })}{' '}
         </Accordion>
       ) : null}
 
@@ -172,43 +148,28 @@ function ContactTrace(props) {
         </Modal.Header>
         <Modal.Body>
           <label>The date you came in contact with them</label>
-          <input
-            type="date"
-            defaultValue={update ? date : null}
-            onChange={(event) => setDate(event.target.value)}
-          />
+          <input type='date' defaultValue={update ? date : null} onChange={(event) => setDate(event.target.value)} />
           <label>Enter the location info</label>
-          <input
-            type="text"
-            defaultValue={update ? locationDescription : null}
-            onChange={(event) => setLocationDescription(event.target.value)}
-          />
+          <input type='text' defaultValue={update ? locationDescription : null} onChange={(event) => setLocationDescription(event.target.value)} />
           <label>Enter all the people's email</label>
           {emails.map((value, index) => {
             return (
               <div key={index}>
                 <input
-                  type="email"
-                  name="email"
-                  placeholder="email"
+                  type='email'
+                  name='email'
+                  placeholder='email'
                   value={value.email}
-                  className={styles[emails.length !== 1 && "email-input-trace"]}
+                  className={styles[emails.length !== 1 && 'email-input-trace']}
                   onChange={(event) => handleEmailFieldChange(event, index)}
                 />
-                {emails.length !== 1 && (
-                  <input
-                    type="button"
-                    value="X"
-                    className={styles["delete-button-trace"]}
-                    onClick={() => handleDeleteEmailField(index)}
-                  />
-                )}
+                {emails.length !== 1 && <input type='button' value='X' className={styles['delete-button-trace']} onClick={() => handleDeleteEmailField(index)} />}
               </div>
             );
           })}
 
           <div>
-            <Button value="Add email" onClick={handleAddEmailField}>
+            <Button value='Add email' onClick={handleAddEmailField}>
               Add email
             </Button>
 
