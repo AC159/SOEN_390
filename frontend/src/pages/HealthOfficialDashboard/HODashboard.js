4d139e1de90389e37a297d749093e3a4e6bf0249
BunnyPrince <jananee1999@yahoo.com>
2022-03-24 20:20:30 -0400

    #89 changed the position of the notification box

--- frontend/src/pages/HealthOfficialDashboard/HODashboard.js
+++ frontend/src/pages/HealthOfficialDashboard/HODashboard.js
@@ -4,6 +4,8 @@ import Dashboard from '../../features/Dashboard/Dashboard';
 import Home from '../../components/Tabs/Home';
 import PatientList from '../../components/Tabs/AdminTabs/PatientList';
 
+import NotificationBox from '../../components/NotificationBox/NotificationBox';
+
 const tabs = [
   {
     eventKey: 'home',
@@ -14,6 +16,11 @@ const tabs = [
     eventKey: 'monitor-patient',
     title: 'Monitor Patients',
     element: <PatientList />
+  },
+    {
+    eventKey: 'notification',
+    title: 'Notification',
+    element: <NotificationBox />
   },
   {
     eventKey: 'contact',
