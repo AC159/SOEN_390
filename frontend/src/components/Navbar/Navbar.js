fdcddab9db640864f046ba94ecadba1cfca3cc78
p-carrier <p-carrier@users.noreply.github.com>
2022-03-31 18:55:08 -0400

    add missing test to Navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,14 +1,13 @@
-import { Link, useNavigate } from "react-router-dom";
-import MainLogo from "../../assets/MainLogo.png";
-import UserIcon from "../../assets/user.png";
-import { useAuth } from "../Authentication/FirebaseAuth/FirebaseAuth";
-import styles from "./Navbar.module.css";
-
+import {Link, useNavigate} from 'react-router-dom';
+import MainLogo from '../../assets/MainLogo.png';
+import UserIcon from '../../assets/user.png';
+import {useAuth} from '../Authentication/FirebaseAuth/FirebaseAuth';
+import styles from './Navbar.module.css';
 
 function Navbar(props) {
   let navigate = useNavigate();
-  let { currentUser, logout } = useAuth();
-  console.log("Navbar user: ", currentUser);
+  let {currentUser, logout} = useAuth();
+  console.log('Navbar user: ', currentUser);
   let isPending = true;
   console.log(isPending);
 
@@ -16,7 +15,7 @@ function Navbar(props) {
     try {
       const response = await logout();
       console.log(response);
-      navigate("/", { replace: true });
+      navigate('/', {replace: true});
     } catch (error) {
       console.log(error);
     }
@@ -24,25 +23,17 @@ function Navbar(props) {
 
   return (
     <nav>
-      <div className={styles["Header"]}>
-        <div className={styles["HeaderInnerContainer"]}>
-          <img src={MainLogo} width="300" alt="CoviCare logo" />
+      <div className={styles['Header']}>
+        <div className={styles['HeaderInnerContainer']}>
+          <img src={MainLogo} width='300' alt='CoviCare logo' />
 
-          <div className={styles["user-navbar-display"]}>
-            <p>
-              Welcome{" "}
-              {currentUser.user !== undefined ? currentUser.user.email : null}
-            </p>
-            <Link to="/user-profile">
-              <img
-                className={styles["user-icon"]}
-                src={UserIcon}
-                width="100"
-                alt="User icon"
-              />
+          <div className={styles['user-navbar-display']}>
+            <p>Welcome {currentUser.user !== undefined ? currentUser.user.email : null}</p>
+            <Link to='/user-profile'>
+              <img className={styles['user-icon']} src={UserIcon} width='100' alt='User icon' />
             </Link>
 
-            <button className={styles["button"]} onClick={handleLogout}>
+            <button className={styles['button']} onClick={handleLogout}>
               Log out
             </button>
           </div>
