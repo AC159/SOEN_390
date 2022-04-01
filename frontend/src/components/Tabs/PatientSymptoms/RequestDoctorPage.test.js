7b65a28a18171054f744a5f9313b137a8364b1f6
p-carrier <p-carrier@users.noreply.github.com>
2022-04-01 14:10:46 -0400

    remove unused import

--- frontend/src/components/Tabs/PatientSymptoms/RequestDoctorPage.test.js
+++ frontend/src/components/Tabs/PatientSymptoms/RequestDoctorPage.test.js
@@ -1,5 +1,4 @@
 import {render, screen} from '@testing-library/react';
-import {BrowserRouter} from 'react-router-dom';
 import axios from 'axios';
 import userEvent from '@testing-library/user-event';
 
