4b3a0793796755efd00506d1aab1530b062ba6c5
p-carrier <p-carrier@users.noreply.github.com>
2022-04-01 10:23:48 -0400

    remove unused import

--- frontend/src/components/Tabs/AdminTabs/RoleRequest.test.js
+++ frontend/src/components/Tabs/AdminTabs/RoleRequest.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import axios from 'axios';
 
 import {AuthContext} from '../../Authentication/FirebaseAuth/FirebaseAuth';
