8c26343814fe4634d47b1dbc7439928359d5f1c6
p-carrier <p-carrier@users.noreply.github.com>
2022-04-01 10:56:57 -0400

    add dashboard component test

---
+++ frontend/src/features/Dashboard/Dashboard.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import Dashboard from './Dashboard';
+import {AuthContext} from '../../components/Authentication/FirebaseAuth/FirebaseAuth';
+import {BrowserRouter} from 'react-router-dom';
+
+describe('visual test of Dashboard component', () => {
+  it('should render and load without error', () => {
+    render(
+      <BrowserRouter>
+        <AuthContext.Provider
+          value={{
+            currentUser: {
+              dbData: {
+                userType: 'healthOfficial',
+              },
+            },
+          }}
+        >
+          <Dashboard
+            defaultActiveKey='home'
+            tabsList={[
+              {
+                eventKey: 'home',
+                title: 'Home',
+                element: <div>Hello</div>,
+              },
+            ]}
+          />
+        </AuthContext.Provider>
+      </BrowserRouter>,
+    );
+
+    expect(screen.getByText(/Hello/)).toBeInTheDocument();
+  });
+});
