c6d3f62c697cce0ecb4859a62a2ad217e75e2f2b
p-carrier <p-carrier@users.noreply.github.com>
2022-04-01 11:50:53 -0400

    add test to appointment component

--- frontend/src/components/Tabs/DoctorTabs/Appointment.test.js
+++ frontend/src/components/Tabs/DoctorTabs/Appointment.test.js
@@ -1,56 +1,137 @@
-import {render, screen} from '@testing-library/react'
-import {BrowserRouter, MemoryRouter} from 'react-router-dom'
+import {render, screen} from '@testing-library/react';
+import {BrowserRouter} from 'react-router-dom';
+import axios from 'axios';
+import userEvent from '@testing-library/user-event';
 
-import { AuthContext } from '../../Authentication/FirebaseAuth/FirebaseAuth';
+import {AuthContext} from '../../Authentication/FirebaseAuth/FirebaseAuth';
 import Appointment from './Appointment';
+import useFetch from '../../../hook/useFetch';
 
+jest.mock('axios');
+jest.mock('../../../hook/useFetch');
 
 describe('visual test of the component', () => {
-    it('should render book appointment tab without crashing', () => {
-        render(<BrowserRouter>
-            <AuthContext.Provider value={{
-              currentUser: {
-                user: 'doctor1_covicare@gmail.com',
-                uid: 'zXX7Yt2lNSa6BhaLFnbosq4IdS22',
-                userType: 'doctor',
-              },
-            }}>
-              <Appointment/>
-            </AuthContext.Provider>
-          </BrowserRouter>);
-        const appointmentElement = screen.getByTestId('appointment-1');
-        expect(appointmentElement).toBeInTheDocument();
-    });
+  it('should render book appointment tab without crashing', () => {
+    useFetch.mockReturnValue([[], jest.fn()]);
+    render(
+      <BrowserRouter>
+        <AuthContext.Provider
+          value={{
+            currentUser: {
+              user: 'doctor1_covicare@gmail.com',
+              uid: 'zXX7Yt2lNSa6BhaLFnbosq4IdS22',
+              userType: 'doctor',
+            },
+          }}
+        >
+          <Appointment />
+        </AuthContext.Provider>
+      </BrowserRouter>,
+    );
+    const appointmentElement = screen.getByTestId('appointment-1');
+    expect(appointmentElement).toBeInTheDocument();
+  });
 
-    it('renders book appointment button correctly', () => {
-        render(<BrowserRouter>
-            <AuthContext.Provider value={{
-              currentUser: {
-                user: 'doctor1_covicare@gmail.com',
-                uid: 'zXX7Yt2lNSa6BhaLFnbosq4IdS22',
-                userType: 'doctor',
-              },
-            }}>
-              <Appointment/>
-            </AuthContext.Provider>
-          </BrowserRouter>)
-        const appointmentBtnElement = screen.getByTestId('viewBookAppointmentBtn');
-        expect(appointmentBtnElement).toHaveTextContent("Book Appointment");
-    })
-    
-    it('should render list of appointments already booked without crashing', () => {
-        render(<BrowserRouter>
-            <AuthContext.Provider value={{
-              currentUser: {
-                user: 'doctor1_covicare@gmail.com',
-                uid: 'zXX7Yt2lNSa6BhaLFnbosq4IdS22',
-                userType: 'doctor',
-              },
-            }}>
-              <Appointment/>
-            </AuthContext.Provider>
-          </BrowserRouter>);
-        const listAppointmentElement = screen.getByTestId('appointment-list');
-        expect(listAppointmentElement).toBeInTheDocument();
-    });
+  it('renders book appointment button correctly', () => {
+    useFetch.mockReturnValue([[], jest.fn()]);
+    render(
+      <BrowserRouter>
+        <AuthContext.Provider
+          value={{
+            currentUser: {
+              user: 'doctor1_covicare@gmail.com',
+              uid: 'zXX7Yt2lNSa6BhaLFnbosq4IdS22',
+              userType: 'doctor',
+            },
+          }}
+        >
+          <Appointment />
+        </AuthContext.Provider>
+      </BrowserRouter>,
+    );
+    const appointmentBtnElement = screen.getByTestId('viewBookAppointmentBtn');
+    expect(appointmentBtnElement).toHaveTextContent('Book Appointment');
+  });
+
+  it('should render empty list of appointments without crashing', async () => {
+    useFetch.mockReturnValue([[], jest.fn()]);
+    render(
+      <BrowserRouter>
+        <AuthContext.Provider
+          value={{
+            currentUser: {
+              user: 'doctor1_covicare@gmail.com',
+              uid: 'zXX7Yt2lNSa6BhaLFnbosq4IdS22',
+              userType: 'doctor',
+            },
+          }}
+        >
+          <Appointment />
+        </AuthContext.Provider>
+      </BrowserRouter>,
+    );
+    const listAppointmentElement = screen.getByTestId('appointment-list');
+    expect(listAppointmentElement).toBeInTheDocument();
+    expect(await screen.findByText(/You have no appointments at this time/)).toBeInTheDocument();
+  });
+
+  it('should render list of appointments already booked without crashing', async () => {
+    const mockFetchAppointment = jest.fn();
+    useFetch.mockReturnValueOnce([[], jest.fn()]).mockReturnValueOnce([
+      [
+        {
+          patientName: 'John Doe',
+          title: 'Amazing Appoitment',
+          information: 'info',
+          dateAndTime: 1234567890,
+        },
+      ],
+      mockFetchAppointment,
+    ]);
+
+    render(
+      <BrowserRouter>
+        <AuthContext.Provider
+          value={{
+            currentUser: {
+              user: 'doctor1_covicare@gmail.com',
+              uid: 'zXX7Yt2lNSa6BhaLFnbosq4IdS22',
+              userType: 'doctor',
+            },
+          }}
+        >
+          <Appointment />
+        </AuthContext.Provider>
+      </BrowserRouter>,
+    );
+
+    expect(await screen.findByText(/^Patient:$/)).toBeInTheDocument();
+    expect(await screen.findByText(/^Title:$/)).toBeInTheDocument();
+    expect(await screen.findByText(/^Info:$/)).toBeInTheDocument();
+    expect(await screen.findByText(/^Date and Time:$/)).toBeInTheDocument();
+    expect(await screen.findByText(/John Doe/)).toBeInTheDocument();
+    expect(await screen.findByText(/Amazing/)).toBeInTheDocument();
+  });
+
+  it('should be able to submit an appointment', () => {
+    useFetch.mockReturnValue([[], jest.fn()]);
+    render(
+      <BrowserRouter>
+        <AuthContext.Provider
+          value={{
+            currentUser: {
+              user: 'doctor1_covicare@gmail.com',
+              uid: 'zXX7Yt2lNSa6BhaLFnbosq4IdS22',
+              userType: 'doctor',
+            },
+          }}
+        >
+          <Appointment />
+        </AuthContext.Provider>
+      </BrowserRouter>,
+    );
+
+    userEvent.click(screen.getByText(/Book Appointment/));
+    expect(axios.post).toHaveBeenCalledTimes(1);
+  });
 });
