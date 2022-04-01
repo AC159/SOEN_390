c30d679eb09e4fb595f8a7b98769263729979af7
p-carrier <p-carrier@users.noreply.github.com>
2022-04-01 10:46:43 -0400

    add RoleRequestBox component test

--- frontend/src/components/AdminComponents/RoleRequestBox.js
+++ frontend/src/components/AdminComponents/RoleRequestBox.js
@@ -1,70 +1,75 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import styles from "./RoleRequestBox.module.css";
-import {Button} from "react-bootstrap";
-import {Card} from "react-bootstrap";
-import axios from "axios";
-import {useState} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from './RoleRequestBox.module.css';
+import {Button} from 'react-bootstrap';
+import {Card} from 'react-bootstrap';
+import axios from 'axios';
+import {useState} from 'react';
 
-function RoleRequestBox(props) {
+function RoleRequestBox({uID, adminID, userEmail, RequesterUsername, RequestType}) {
   function makeTitle(RequestType) {
-    return RequestType + " Request";
+    return RequestType + ' Request';
   }
 
   const [displayRequestBox, setDisplayRB] = useState(true);
 
   const approveUser = async () => {
     const approveInfo = {
-      userId: props.uID,
-      adminId: props.adminID,
-      userEmail: props.userEmail,
-      message: `Hi ${props.RequesterUsername}, \nThis email is to let you know that your account has been approved!` // email can be plain text or html
-    }
+      userId: uID,
+      adminId: adminID,
+      userEmail: userEmail,
+      message: `Hi ${RequesterUsername}, \nThis email is to let you know that your account has been approved!`, // email can be plain text or html
+    };
 
-    axios.post(`admin/${props.adminID}/approve-user`, approveInfo)
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error.response);
-    })
+    axios
+      .post(`admin/${adminID}/approve-user`, approveInfo)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
 
     setDisplayRB(false);
-  }
+  };
 
   const rejectUser = () => {
     const rejectInfo = {
-      userId: props.uID,
-      adminId: props.adminID
-    }
+      userId: uID,
+      adminId: adminID,
+    };
 
-    axios.post(`admin/${props.adminID}/reject-user`, rejectInfo)
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error.response);
-    })
+    axios
+      .post(`admin/${adminID}/reject-user`, rejectInfo)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
     setDisplayRB(false);
-  }
+  };
 
   return (
-    <div className={styles["card-container"]}>
-      {
-        (displayRequestBox) ? 
-        
-          <Card className={styles["card"]}>
+    <div className={styles['card-container']}>
+      {displayRequestBox ? (
+        <Card className={styles['card']}>
           <Card.Body>
-          <Card.Title>{makeTitle(props.RequestType)}</Card.Title>
-          <Card.Text>
-            {props.RequesterUsername} wants to join as a {props.RequestType}
-          </Card.Text>
-          <Button variant="primary" onClick={approveUser}>Approve</Button>{" "}
-          <Button variant="danger" onClick={rejectUser}>Reject</Button>{" "}
-          <Button variant="info">More Info</Button>{" "}
-        </Card.Body>
-      </Card> 
-          : <div></div>
-      }
+            <Card.Title>{makeTitle(RequestType)}</Card.Title>
+            <Card.Text>
+              {RequesterUsername} wants to join as a {RequestType}
+            </Card.Text>
+            <Button variant='primary' onClick={approveUser}>
+              Approve
+            </Button>{' '}
+            <Button variant='danger' onClick={rejectUser}>
+              Reject
+            </Button>{' '}
+            <Button variant='info'>More Info</Button>{' '}
+          </Card.Body>
+        </Card>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
