18c9582648e92f396ba55b78f5ef89e89e224494
p-carrier <p-carrier@users.noreply.github.com>
2022-04-13 17:25:34 -0400

    add test to Patient Dashboard page

---
+++ frontend/src/pages/PatientDashboard/PatientDashboard.test.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import {BrowserRouter} from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import axios from 'axios';
+
+import {AuthContext} from '../../components/Authentication/FirebaseAuth/FirebaseAuth';
+import PatientDashboard from './PatientDashboard';
+
+jest.mock('axios');
+
+describe('test PatientDashboard page', () => {
+  const auth = {
+    currentUser: {
+      user: {
+        uid: '1234',
+      },
+    },
+  };
+  it('should load and display without error', () => {
+    render(
+      <BrowserRouter>
+        <AuthContext.Provider value={auth}>
+          <PatientDashboard />
+        </AuthContext.Provider>
+      </BrowserRouter>,
+    );
+
+    expect(screen.getByText(/Today's News/)).toBeInTheDocument();
+  });
+
+  // it('should be able to change tab', async () => {
+  //   axios.get.mockResolvedValue({
+  //     data: {
+  //       users: [],
+  //       doctors: [],
+  //       healthOfficials: [],
+  //       immigrationOfficials: [],
+  //       data: [],
+  //     },
+  //   });
+  //   render(
+  //     <BrowserRouter>
+  //       <AuthContext.Provider value={auth}>
+  //         <PatientDashboard />
+  //       </AuthContext.Provider>
+  //     </BrowserRouter>,
+  //   );
+
+  //   expect(screen.getByText(/Today's News/)).toBeInTheDocument();
+  //   userEvent.click(await screen.findByText(/^Account Request$/));
+  //   expect(await screen.findByText(/^Role Requests$/)).toBeInTheDocument();
+  //   expect(await screen.findByText(/^Patient Requests$/)).toBeInTheDocument();
+  //   userEvent.click(await screen.findByText(/^Patient List$/));
+  //   expect(await screen.findByPlaceholderText('Search patient by name...')).toBeInTheDocument();
+  //   userEvent.click(await screen.findByText(/^Doctor List$/));
+  //   expect(await screen.findByPlaceholderText('Search doctor by name...')).toBeInTheDocument();
+  //   userEvent.click(await screen.findByText(/^Contact$/));
+  //   expect(await screen.findByText('Contacts')).toBeInTheDocument();
+  // });
+});
