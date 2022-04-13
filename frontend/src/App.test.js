a91bd8e5692e92c8fde3869de21c8014768066e9
p-carrier <p-carrier@users.noreply.github.com>
2022-04-13 17:26:14 -0400

    add test to App file

---
+++ frontend/src/App.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import {BrowserRouter} from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import axios from 'axios';
+
+import {AuthContext} from './components/Authentication/FirebaseAuth/FirebaseAuth';
+import App from './App';
+
+test('it should load and display without error', () => {
+  const auth = {
+    currentUser: {
+      user: {
+        uid: '1234',
+      },
+    },
+  };
+
+  render(
+    <BrowserRouter>
+      <AuthContext.Provider value={auth}>
+        <App />
+      </AuthContext.Provider>
+    </BrowserRouter>,
+  );
+
+  expect(screen.getByText(/CoviCare/)).toBeInTheDocument();
+});
