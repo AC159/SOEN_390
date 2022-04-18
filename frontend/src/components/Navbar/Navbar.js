51b5fea607164fb3c4b44736af8555df2b7baac4
Anas Peerzada <apeerzada987@gmail.com>
2022-04-17 22:39:53 -0400

    Fixed redirection when CoviCare logo is clicked

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -2,14 +2,41 @@ import {Link, useNavigate} from 'react-router-dom';
 import MainLogo from '../../assets/MainLogo.png';
 import UserIcon from '../../assets/user.png';
 import {useAuth} from '../Authentication/FirebaseAuth/FirebaseAuth';
+import {useState, useEffect} from 'react';
 import styles from './Navbar.module.css';
 
 function Navbar(props) {
   let navigate = useNavigate();
   let {currentUser, logout} = useAuth();
+  let [userLink, setUserLink] = useState('/');
   console.log('Navbar user: ', currentUser);
   let isPending = true;
   console.log(isPending);
+
+  useEffect(() => {
+    console.log(userLink);
+    if(currentUser.dbData.userStatus === null)
+      setUserLink('/general-dashboard');
+    else
+    {
+      if(currentUser.dbData.userStatus == "REJECTED") 
+        setUserLink('/general-dashboard');
+      else if(currentUser.dbData.userStatus == "APPROVED") 
+      {
+        switch(currentUser.dbData.userType)
+        {
+          case 'administrator': setUserLink('/admin-dashboard'); break;
+          case 'doctor': setUserLink('/doctor-dashboard'); break;
+          case 'patient': setUserLink('/patient-dashboard'); break;
+          case 'healthOfficial': setUserLink('/health-official-dashboard'); break;
+          case 'immigrationOfficial': setUserLink('/immigration-officer-dashboard'); break;
+
+          default: setUserLink('/general-dashboard'); break;
+        }
+      }
+    }
+    console.log(userLink);
+  }, [currentUser]);
 
   const handleLogout = async () => {
     try {
@@ -21,11 +48,13 @@ function Navbar(props) {
     }
   };
 
+  console.log(userLink);
+
   return (
     <nav>
       <div className={styles['Header']}>
         <div className={styles['HeaderInnerContainer']}>
-          <Link to='/health-official-dashboard'>
+          <Link to={userLink}>
             <img src={MainLogo} className={styles['logo-navbar']} alt='CoviCare logo' />
           </Link>
 
